e3c7d66cca4ea9fd625547d2639cc1482b9aba91
Kiki2811 <134044627+Kiki2811@users.noreply.github.com>
2025-09-25 17:01:42 +0200

    Add files via upload

---
+++ pytorch-kfac-cola/torch_kfac/layers/fisher_block.py
@@ -0,0 +1,172 @@
+from typing import Iterable, Optional, Tuple
+import torch
+
+from ..utils import MovingAverageVariable, Lock, compute_pi_adjusted_damping, \
+    inverse_by_cholesky, kronecker_product, normalize_damping
+
+from cola.ops import LinearOperator
+import torch
+
+class FisherBlockOperator(LinearOperator):
+    def __init__(self, activation_cov: torch.Tensor, sensitivity_cov: torch.Tensor, renorm_coeff: float):
+        self.activation_cov = activation_cov
+        self.sensitivity_cov = sensitivity_cov
+        self.renorm_coeff = renorm_coeff
+
+    def matvec(self, x):
+        # Implement the matrix-vector multiplication using the Fisher block
+        return self.sensitivity_cov @ x @ self.activation_cov / self.renorm_coeff
+
+class FisherBlock(object):
+    def __init__(self, in_features: int, out_features: int, dtype: torch.dtype, device: torch.device, **kwargs):
+        self._in_features = in_features
+        self._out_features = out_features
+        self._dtype = dtype
+        self._device = device
+
+        self._activations_cov = MovingAverageVariable((in_features, in_features), dtype=dtype, device=device)
+        self._sensitivities_cov = MovingAverageVariable((out_features, out_features), dtype=dtype, device=device)
+
+        self._forward_lock = False
+        self._backward_lock = False
+        self._enable_pi_correction = True
+
+    def setup(self, forward_lock: Lock, backward_lock: Lock, enable_pi_correction: bool, **kwargs) -> None:
+        self._forward_lock = forward_lock
+        self._backward_lock = backward_lock
+        self._enable_pi_correction = enable_pi_correction
+
+    def update_cov(self, cov_ema_decay: float = 1.0) -> None:
+        raise NotImplementedError()
+
+    def compute_damping(self, damping: torch.Tensor, normalization: float = None) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Calculates the adapted damping for the Tikhonov regularization.
+        Args:
+            damping: the damping constant for the Tikhonov
+            normalization: If true, the damping will be normalized using this parameter.
+
+        Returns:
+            A tuple where the first entry is the damping for the activations covariance matrix,
+            the second entry is the damping for the sensitivities covariance matrix.
+        """
+        if normalization is not None:
+            maybe_normalized_damping = normalize_damping(damping, normalization)
+        else:
+            maybe_normalized_damping = damping
+
+        if self._enable_pi_correction:
+            return compute_pi_adjusted_damping(
+                self.activation_covariance,
+                self.sensitivity_covariance,
+                maybe_normalized_damping ** 0.5
+            )
+        else:
+            return maybe_normalized_damping ** 0.5, maybe_normalized_damping ** 0.5
+
+    def full_fisher_block(self):
+        left_factor = self.activation_covariance
+        right_factor = self.sensitivity_covariance
+        return self._renorm_coeff * kronecker_product(left_factor, right_factor)
+    
+    def reset(self) -> None:
+        self._activations_cov.reset()
+        self._sensitivities_cov.reset()
+
+    def grads_to_mat(self, grads: Iterable[torch.Tensor]) -> torch.Tensor:
+        raise NotImplementedError()
+
+    def mat_to_grads(self, mat_grads: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError()
+
+    def multiply(self, grads: Iterable[torch.Tensor], damping: torch.Tensor) -> Iterable[torch.Tensor]:
+        act_cov, sen_cov = self.activation_covariance, self.sensitivity_covariance
+        a_damp, s_damp = self.compute_damping(damping, self.renorm_coeff)
+
+        # Create the CoLA operator with damping applied
+        operator = FisherBlockOperator(act_cov + torch.eye(act_cov.shape[0], device=a_damp.device) * a_damp,
+                                      sen_cov + torch.eye(sen_cov.shape[0], device=s_damp.device) * s_damp,
+                                      self.renorm_coeff)
+        
+        # Apply CoLA's matvec function for the matrix-vector multiplication
+        mat_grads = self.grads_to_mat(grads)
+        nat_grads = operator.matvec(mat_grads)
+        
+        return self.mat_to_grads(nat_grads)
+
+        
+    def multiply_preconditioner(self, grads: Iterable[torch.Tensor], damping: torch.Tensor) -> Iterable[torch.Tensor]:
+        act_cov, sen_cov = self.activation_covariance, self.sensitivity_covariance
+        a_damp, s_damp = self.compute_damping(damping, self.renorm_coeff)
+
+        # Create inverse operators for CoLA
+        act_cov_inverse = inverse_by_cholesky(act_cov, a_damp)
+        sen_cov_inverse = inverse_by_cholesky(sen_cov, s_damp)
+        
+        # Create the CoLA operator for preconditioner
+        operator = FisherBlockOperator(act_cov_inverse, sen_cov_inverse, self.renorm_coeff)
+        
+        mat_grads = self.grads_to_mat(grads)
+        nat_grads = operator.matvec(mat_grads)
+        
+        return self.mat_to_grads(nat_grads)
+
+    def get_fisher_components(self) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor]]:
+        if hasattr(self, '_activations_cov') and hasattr(self, '_sensitivities_cov'):
+            a_cov = self._activations_cov._var
+            s_cov = self._sensitivities_cov._var
+            return a_cov, s_cov
+        return None, None
+
+    @property
+    def is_static(self) -> bool:
+        return (self._in_features == 0) or (self._out_features == 0)
+        
+    @property
+    def activation_covariance(self) -> torch.Tensor:
+        return self._activations_cov.value
+
+    @property
+    def sensitivity_covariance(self) -> torch.Tensor:
+        return self._sensitivities_cov.value
+
+    @property
+    def vars(self) -> Iterable[torch.Tensor]:
+        raise NotImplementedError()
+
+    @property
+    def grads(self) -> Iterable[torch.Tensor]:
+        return [tensor.grad for tensor in self.vars]
+
+    def set_gradients(self, new_grads):
+        for var, grad in zip(self.vars, new_grads):
+            var.grad.data = grad
+    
+    @property
+    def renorm_coeff(self) -> float:
+        return 1.
+
+
+class ExtensionFisherBlock(FisherBlock):
+    def __init__(self, module: torch.nn.Module, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.module = module
+        self.forward_hook_handle = None
+        self.backward_hook_handle = None
+
+        self.forward_hook_handle = self.module.register_forward_hook(self._forward_hook_wrapper)
+        self.backward_hook_handle = self.module.register_backward_hook(self._backward_hook_wrapper)
+
+    def _forward_hook_wrapper(self, *args):
+        if self._forward_lock:
+            return self.forward_hook(*args)
+
+    def _backward_hook_wrapper(self, *args):
+        if self._backward_lock:
+            return self.backward_hook(*args)
+
+    def forward_hook(self, module: torch.nn.Module, *args):
+        raise NotImplementedError()
+
+    def backward_hook(self, module: torch.nn.Module, *args):
+        raise NotImplementedError()
